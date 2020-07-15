75901627e86e3790733036d71c280e985a9c41b4
Felipe Pabon <felipe.pabon@belltech.la>
2020-07-15 08:11:04 -0500

    Creación Capa Application

---
+++ PacienteES.Application/Abstract/IPacienteService.cs
@@ -0,0 +1,12 @@
+﻿using PacienteES.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacienteES.Application.Abstract
+{
+    public interface IPacienteService
+    {
+        void Create(Paciente paciente);
+    }
+}
---
+++ PacienteES.Application/Implements/PacienteService.cs
@@ -0,0 +1,32 @@
+﻿using PacienteES.Application.Abstract;
+using PacienteES.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnitOfWork;
+
+namespace PacienteES.Application.Implements
+{
+    class PacienteService : IPacienteService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PacienteService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public void Create(Paciente paciente)
+        {
+            throw new NotImplementedException();
+        }
+
+        /*public void Create(UserExample model)
+        {
+            // Call to your repository
+            _uow.Repository.UserRepository.Add(model);
+
+            // Save changes
+            _uow.SaveChanges();
+        }*/
+    }
+}
